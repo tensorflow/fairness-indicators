b47857fd61fecf6c128fbdea30b1ce686339dd51
Christina Greer <ckuhn@google.com>
2019-12-06 14:34:30 -0800

    Some utilities for querying evaluation results for specific values in relation to a baseline.
    PiperOrigin-RevId: 284264174

---
+++ fairness_indicators/remediation/weight_utils.py
@@ -0,0 +1,71 @@
+# Lint as: python3
+"""Utilities to suggest weights based on model analysis results."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import google_type_annotations
+from __future__ import print_function
+
+
+def create_percentage_difference_dictionary(eval_result, baseline_name,
+                                            metric_name):
+  """Creates dictionary of a % difference between a baseline and other slices.
+
+  Args:
+    eval_result: Loaded eval result from running TensorFlow Model Analysis.
+    baseline_name: Name of the baseline slice, 'Overall' or a specified tuple.
+    metric_name: Name of the metric on which to perform comparisons.
+
+  Returns:
+    Dictionary mapping slices to percentage difference from the baseline slice.
+  """
+  baseline_value = get_baseline_value(eval_result, baseline_name, metric_name)
+  difference = {}
+  for metrics_tuple in eval_result.slicing_metrics:
+    slice_key = metrics_tuple[0]
+    metrics = metrics_tuple[1]
+    # Concatenate feature name/values for intersectional features.
+    column = '-'.join([elem[0] for elem in slice_key])
+    feature_val = '-'.join([elem[1] for elem in slice_key])
+    if column not in difference:
+      difference[column] = {}
+    difference[column][feature_val] = (_get_metric_value(metrics, metric_name)
+                                       - baseline_value) / baseline_value
+  return difference
+
+
+def _get_metric_value(nested_dict, metric_name):
+  """Gets the value of the named metric from a slice's metrics."""
+  for key in nested_dict:
+    if metric_name in nested_dict[key]['']:
+      typed_value = nested_dict[key][''][metric_name]
+      if 'doubleValue' in typed_value:
+        return typed_value['doubleValue']
+      if 'boundedValue' in typed_value:
+        return typed_value['boundedValue']['value']
+      raise ValueError('Unsupported value type: %s' % typed_value)
+    else:
+      raise ValueError('Key %s not found in %s' %
+                       (metric_name, nested_dict[key][''].keys()))
+
+
+def get_baseline_value(eval_result, baseline_name, metric_name):
+  """Looks through the evaluation result for the value of the baseline slice.
+
+  Args:
+    eval_result: Loaded eval result from running TensorFlow Model Analysis.
+    baseline_name: Name of the baseline slice, 'Overall' or a specified tuple.
+    metric_name: Name of the metric on which to perform comparisons.
+
+  Returns:
+    Dictionary mapping slices to percentage difference from the baseline slice.
+  """
+  for metrics_tuple in eval_result.slicing_metrics:
+    slice_tuple = metrics_tuple[0]
+    if baseline_name == 'Overall':
+      if not slice_tuple:
+        return _get_metric_value(metrics_tuple[1], metric_name)
+    if baseline_name == slice_tuple:
+      return _get_metric_value(metrics_tuple[1], metric_name)
+  raise ValueError('Could not find baseline %s in eval_result: %s' %
+                   (baseline_name, eval_result))
---
+++ fairness_indicators/remediation/weight_utils_test.py
@@ -0,0 +1,198 @@
+# Lint as: python3
+"""Tests for fairness_indicators.remediation.weight_utils."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+
+# Standard imports
+from fairness_indicators.remediation import weight_utils
+import tensorflow as tf
+
+
+EvalResult = collections.namedtuple('EvalResult', ['slicing_metrics'])
+
+
+class WeightUtilsTest(tf.test.TestCase):
+
+  def create_eval_result(self):
+    return EvalResult(slicing_metrics=[
+        ((), {
+            '': {
+                '': {
+                    'post_export_metrics/negative_rate@0.10': {
+                        'doubleValue': 0.08
+                    },
+                    'accuracy': {
+                        'doubleValue': 0.444
+                    }
+                }
+            }
+        }),
+        ((('gender', 'female'),), {
+            '': {
+                '': {
+                    'post_export_metrics/negative_rate@0.10': {
+                        'doubleValue': 0.09
+                    },
+                    'accuracy': {
+                        'doubleValue': 0.333
+                    }
+                }
+            }
+        }),
+        (((u'gender', u'female'),
+          (u'sexual_orientation', u'homosexual_gay_or_lesbian')), {
+              '': {
+                  '': {
+                      'post_export_metrics/negative_rate@0.10': {
+                          'doubleValue': 0.1
+                      },
+                      'accuracy': {
+                          'doubleValue': 0.222
+                      }
+                  }
+              }
+          }),
+    ])
+
+  def create_bounded_result(self):
+    return EvalResult(slicing_metrics=[
+        ((), {
+            '': {
+                '': {
+                    'post_export_metrics/negative_rate@0.10': {
+                        'boundedValue': {
+                            'lowerBound': 0.07,
+                            'upperBound': 0.09,
+                            'value': 0.08,
+                            'methodology': 'POISSON_BOOTSTRAP'
+                        }
+                    },
+                    'accuracy': {
+                        'boundedValue': {
+                            'lowerBound': 0.07,
+                            'upperBound': 0.09,
+                            'value': 0.444,
+                            'methodology': 'POISSON_BOOTSTRAP'
+                        }
+                    }
+                }
+            }
+        }),
+        ((('gender', 'female'),), {
+            '': {
+                '': {
+                    'post_export_metrics/negative_rate@0.10': {
+                        'boundedValue': {
+                            'lowerBound': 0.07,
+                            'upperBound': 0.09,
+                            'value': 0.09,
+                            'methodology': 'POISSON_BOOTSTRAP'
+                        }
+                    },
+                    'accuracy': {
+                        'boundedValue': {
+                            'lowerBound': 0.07,
+                            'upperBound': 0.09,
+                            'value': 0.333,
+                            'methodology': 'POISSON_BOOTSTRAP'
+                        }
+                    }
+                }
+            }
+        }),
+        (((u'gender', u'female'),
+          (u'sexual_orientation', u'homosexual_gay_or_lesbian')), {
+              '': {
+                  '': {
+                      'post_export_metrics/negative_rate@0.10': {
+                          'boundedValue': {
+                              'lowerBound': 0.07,
+                              'upperBound': 0.09,
+                              'value': 0.1,
+                              'methodology': 'POISSON_BOOTSTRAP'
+                          }
+                      },
+                      'accuracy': {
+                          'boundedValue': {
+                              'lowerBound': 0.07,
+                              'upperBound': 0.09,
+                              'value': 0.222,
+                              'methodology': 'POISSON_BOOTSTRAP'
+                          }
+                      }
+                  }
+              }
+          }),
+    ])
+
+  def test_baseline(self):
+    test_eval_result = self.create_eval_result()
+    self.assertEqual(
+        0.08,
+        weight_utils.get_baseline_value(
+            test_eval_result, 'Overall',
+            'post_export_metrics/negative_rate@0.10'))
+    self.assertEqual(
+        0.09,
+        weight_utils.get_baseline_value(
+            test_eval_result, (('gender', 'female'),),
+            'post_export_metrics/negative_rate@0.10'))
+    # Test 'accuracy'.
+    self.assertEqual(
+        0.444,
+        weight_utils.get_baseline_value(test_eval_result, 'Overall',
+                                        'accuracy'))
+    # Test intersectional metrics.
+    self.assertEqual(
+        0.222,
+        weight_utils.get_baseline_value(
+            test_eval_result,
+            ((u'gender', u'female'),
+             (u'sexual_orientation', u'homosexual_gay_or_lesbian')),
+            'accuracy'))
+    with self.assertRaises(ValueError):
+      # Test slice not found.
+      weight_utils.get_baseline_value(test_eval_result,
+                                      (('nonexistant', 'slice'),), 'accuracy')
+      # Test metric not found.
+      weight_utils.get_baseline_value(test_eval_result, (('gender', 'female'),),
+                                      'nonexistent_metric')
+
+  def test_create_difference_dictionary(self):
+    test_eval_result = self.create_eval_result()
+    res = weight_utils.create_percentage_difference_dictionary(
+        test_eval_result, 'Overall', 'post_export_metrics/negative_rate@0.10')
+    self.assertEqual(3, len(res))
+    self.assertIn('gender-sexual_orientation', res)
+    self.assertIn('gender', res)
+    self.assertAlmostEqual(res['gender']['female'], 0.125)
+    self.assertAlmostEqual(res[''][''], 0)
+
+  def test_create_difference_dictionary_baseline(self):
+    test_eval_result = self.create_eval_result()
+    res = weight_utils.create_percentage_difference_dictionary(
+        test_eval_result, (('gender', 'female'),),
+        'post_export_metrics/negative_rate@0.10')
+    self.assertEqual(3, len(res))
+    self.assertIn('gender-sexual_orientation', res)
+    self.assertIn('gender', res)
+    self.assertAlmostEqual(res['gender']['female'], 0)
+    self.assertAlmostEqual(res[''][''], -0.11111111)
+
+  def test_create_difference_dictionary_bounded_metrics(self):
+    test_eval_result = self.create_bounded_result()
+    res = weight_utils.create_percentage_difference_dictionary(
+        test_eval_result, 'Overall', 'post_export_metrics/negative_rate@0.10')
+    self.assertEqual(3, len(res))
+    self.assertIn('gender-sexual_orientation', res)
+    self.assertIn('gender', res)
+    self.assertAlmostEqual(res['gender']['female'], 0.125)
+    self.assertAlmostEqual(res[''][''], 0)
+
+
+if __name__ == '__main__':
+  tf.test.main()
