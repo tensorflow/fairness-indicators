2ef45d50ae2300759c1786dec960077632313c98
Huanming Fang <huanmingf@google.com>
2020-02-21 14:11:44 -0800

    Update the tensorboard plugin ui to match the tfma v 0.21.2
    PiperOrigin-RevId: 296508969

--- tensorboard_plugin/setup.py
+++ tensorboard_plugin/setup.py
@@ -34,7 +34,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='tensorboard_plugin_fairness_indicators',
-    version='0.0.2',
+    version='0.0.3',
     description='Fairness Indicators TensorBoard Plugin',
     long_description=long_description,
     long_description_content_type='text/markdown',
