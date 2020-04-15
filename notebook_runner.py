0e3d6758824d89d6bc229588c645d5440a60471c
Karan Shukla <shuklak13@users.noreply.github.com>
2020-04-15 15:16:55 -0700

    remove timeout

--- notebook_runner.py
+++ notebook_runner.py
@@ -11,7 +11,7 @@ def run_notebook(notebook_path):
     with open(notebook_path) as f:
         nb = nbformat.read(f, as_version=4)
 
-    proc = ExecutePreprocessor(timeout=600, kernel_name='python3')
+    proc = ExecutePreprocessor(timeout=None, kernel_name='python3')
     proc.allow_errors = True
 
     proc.preprocess(nb, {'metadata': {'path': '/'}})
