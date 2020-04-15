385ebb973a80492f33357bb2e24af564150dbc66
Karan Shukla <shuklak13@users.noreply.github.com>
2020-04-15 14:33:33 -0700

    fix test path

--- test_runner.py
+++ test_runner.py
@@ -7,7 +7,7 @@ class TestNotebook(unittest.TestCase):
 
   def test_runner(self):
     errors = notebook_runner.run_notebook(
-        'examples/Fairness_Indicators_Example_Colab.ipynb')
+        'fairness_indicators/examples/Fairness_Indicators_Example_Colab.ipynb')
     self.assertEqual(errors, [])
 
 
