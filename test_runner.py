cb66a17efbb9de17f46d4fba28762534d0ed4e9b
Karan Shukla <shuklak13@users.noreply.github.com>
2020-04-15 20:38:09 -0700

    print errors (test)

--- test_runner.py
+++ test_runner.py
@@ -8,6 +8,9 @@ class TestNotebook(unittest.TestCase):
   def test_runner(self):
     errors = notebook_runner.run_notebook(
         'fairness_indicators/examples/Fairness_Indicators_Example_Colab.ipynb')
+    print('ERRORS (test)')
+    print(type(errors))
+    print(errors)
     self.assertEqual(errors, [])
 
 
