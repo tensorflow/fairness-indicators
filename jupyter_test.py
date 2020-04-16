767e362985ef88448d8bbb11059de598be7f9ca7
Karan Shukla <shuklak13@users.noreply.github.com>
2020-04-15 21:56:24 -0700

    fix _run_notebook

--- jupyter_test.py
+++ jupyter_test.py
@@ -38,37 +38,37 @@ def _run_notebook(path):
 class TestNotebook(unittest.TestCase):
 
   def test_example(self):
-    errors = notebook_runner.run_notebook(
+    errors = _run_notebook(
         'fairness_indicators/examples/Fairness_Indicators_Example_Colab.ipynb')
     self.assertEmpty(errors)
 
 #   def test_facessd(self):
-#     errors = notebook_runner.run_notebook(
+#     errors = _run_notebook(
 #         'fairness_indicators/examples/Facessd_Fairness_Indicators_Example_Colab.ipynb')
 #     self.assertEmpty(errors)
 
 #   def test_lineage(self):
-#     errors = notebook_runner.run_notebook(
+#     errors = _run_notebook(
 #         'fairness_indicators/examples/Fairness_Indicators_Lineage_Case_Study.ipynb')
 #     self.assertEmpty(errors)
 
 #   def test_tb(self):
-#     errors = notebook_runner.run_notebook(
+#     errors = _run_notebook(
 #         'fairness_indicators/examples/Fairness_Indicators_TensorBoard_Plugin_Example_Colab.ipynb')
 #     self.assertEmpty(errors)
 
 #   def test_tfco_celeba(self):
-#     errors = notebook_runner.run_notebook(
+#     errors = _run_notebook(
 #         'fairness_indicators/examples/Fairness_Indicators_TFCO_CelebA_Case_Study.ipynb')
 #     self.assertEmpty(errors)
 
 #   def test_tfco_wiki(self):
-#     errors = notebook_runner.run_notebook(
+#     errors = _run_notebook(
 #         'fairness_indicators/examples/Fairness_Indicators_TFCO_Wiki_Case_Study.ipynb')
 #     self.assertEmpty(errors)
 
 #   def test_tfhub(self):
-#     errors = notebook_runner.run_notebook(
+#     errors = _run_notebook(
 #         'fairness_indicators/examples/Fairness_Indicators_on_TF_Hub_Text_Embeddings.ipynb')
 #     self.assertEmpty(errors)
 
