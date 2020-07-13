e7ab60903925b435fc7d330e6b29439b43a85fe1
Dhruvesh Talati <dhruvesht@google.com>
2020-07-13 16:52:15 -0700

    Updating the setup.py for Fairness Indicators Tensorboard Plugin by adding the version module and removing setuptools dependency.
    PiperOrigin-RevId: 321061505

--- tensorboard_plugin/setup.py
+++ tensorboard_plugin/setup.py
@@ -23,21 +23,22 @@ from setuptools import setup
 
 REQUIRED_PACKAGES = [
     'protobuf >= 3.6.0',
-    'setuptools >= 40.2.0',
     'tensorboard >= 2.1.0, < 3',
     'tensorflow >= 2.1.0, < 3',
     'tensorflow-model-analysis >= 0.21.2, < 1',
-    # python3 specifically requires wheel 0.26
-    'wheel; python_version < "3"',
-    'wheel >= 0.26; python_version >= "3"',
 ]
 
 with open('README.md', 'r', encoding='utf-8') as fh:
   long_description = fh.read()
 
+# Get version from version module.
+with open('tensorboard_plugin_fairness_indicators/version.py') as fp:
+  globals_dict = {}
+  exec(fp.read(), globals_dict)  # pylint: disable=exec-used
+__version__ = globals_dict['__version__']
+
 setup(
     name='tensorboard_plugin_fairness_indicators',
-    version='0.0.5',
     description='Fairness Indicators TensorBoard Plugin',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -53,26 +54,27 @@ setup(
             'fairness_indicators = tensorboard_plugin_fairness_indicators.plugin:FairnessIndicatorsPlugin',
         ],
     },
-    # Disallow python 3.0 and 3.1 which lack a 'futures' module (see above).
-    python_requires='>= 2.7, != 3.0.*, != 3.1.*',
+    python_requires='>=3.5,<4',
     install_requires=REQUIRED_PACKAGES,
     tests_require=REQUIRED_PACKAGES,
-    # PyPI package information.
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
         'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     license='Apache 2.0',
     keywords='tensorflow model analysis fairness indicators tensorboard machine learning',
