8d34f3a8b4abcf27ff452c7202273509ab77f00f
dhruvesh09 <30351549+dhruvesh09@users.noreply.github.com>
2021-05-13 18:10:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,11 +35,11 @@ def select_constraint(default, nightly=None, git_master=None):
 REQUIRED_PACKAGES = [
     'tensorflow>=1.15.2,!=2.0.*,!=2.1.*,!=2.2.*,!=2.4.*,<3',
     'tensorflow-data-validation' + select_constraint(
-        default='>=0.26,<0.27',
+        default='>=0.26.1,<0.27',
         nightly='>=0.27.0.dev',
         git_master='@git+https://github.com/tensorflow/data-validation@master'),
     'tensorflow-model-analysis' + select_constraint(
-        default='>=0.26,<0.27',
+        default='>=0.26.1,<0.27',
         nightly='>=0.27.0.dev',
         git_master='@git+https://github.com/tensorflow/model-analysis@master'),
     'witwidget>=1.4.4,<2',
