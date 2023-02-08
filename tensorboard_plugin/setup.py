d635e8e9dc989784a82621495167613a074ca7af
Raviteja Gorijala <gorijala@google.com>
2023-02-08 11:21:21 -0800

    Internal change
    PiperOrigin-RevId: 508133923

--- tensorboard_plugin/setup.py
+++ tensorboard_plugin/setup.py
@@ -25,8 +25,8 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-if sys.version_info >= (3, 10):
-  sys.exit('Sorry, Python >= 3.10 is not supported')
+if sys.version_info >= (3, 11):
+  sys.exit('Sorry, Python >= 3.11 is not supported')
 
 
 def select_constraint(default, nightly=None, git_master=None):
@@ -79,7 +79,7 @@ setup(
             'fairness_indicators = tensorboard_plugin_fairness_indicators.plugin:FairnessIndicatorsPlugin',
         ],
     },
-    python_requires='>=3.7,<3.10',
+    python_requires='>=3.8,<3.11',
     install_requires=REQUIRED_PACKAGES,
     tests_require=REQUIRED_PACKAGES,
     classifiers=[
@@ -90,9 +90,9 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Mathematics',
