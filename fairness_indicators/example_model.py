5623f4b7ae21b66691a9a3f1d2c6599c4bb4abeb
andrewfulton9 <andrewfulton9@gmail.com>
2025-08-04 17:38:53 +0000

    pre-commit run --all-files

--- fairness_indicators/example_model.py
+++ fairness_indicators/example_model.py
@@ -21,15 +21,13 @@ results can be visualized using tools like TensorBoard.
 
 from typing import Any
 
-from fairness_indicators import fairness_indicators_metrics  # pylint: disable=unused-import
-from tensorflow import keras
 import tensorflow.compat.v1 as tf
 import tensorflow_model_analysis as tfma
+from tensorflow import keras
 
-
-TEXT_FEATURE = 'comment_text'
-LABEL = 'toxicity'
-SLICE = 'slice'
+TEXT_FEATURE = "comment_text"
+LABEL = "toxicity"
+SLICE = "slice"
 FEATURE_MAP = {
     LABEL: tf.io.FixedLenFeature([], tf.float32),
     TEXT_FEATURE: tf.io.FixedLenFeature([], tf.string),
@@ -38,74 +36,75 @@ FEATURE_MAP = {
 
 
 class ExampleParser(keras.layers.Layer):
-  """A Keras layer that parses the tf.Example."""
+    """A Keras layer that parses the tf.Example."""
 
-  def __init__(self, input_feature_key):
-    self._input_feature_key = input_feature_key
-    self.input_spec = keras.layers.InputSpec(shape=(1,), dtype=tf.string)
-    super().__init__()
+    def __init__(self, input_feature_key):
+        self._input_feature_key = input_feature_key
+        self.input_spec = keras.layers.InputSpec(shape=(1,), dtype=tf.string)
+        super().__init__()
 
-  def compute_output_shape(self, input_shape: Any):
-    return [1, 1]
+    def compute_output_shape(self, input_shape: Any):
+        return [1, 1]
 
-  def call(self, serialized_examples):
-    def get_feature(serialized_example):
-      parsed_example = tf.io.parse_single_example(
-          serialized_example, features=FEATURE_MAP
-      )
-      return parsed_example[self._input_feature_key]
-    serialized_examples = tf.cast(serialized_examples, tf.string)
-    return tf.map_fn(get_feature, serialized_examples)
+    def call(self, serialized_examples):
+        def get_feature(serialized_example):
+            parsed_example = tf.io.parse_single_example(
+                serialized_example, features=FEATURE_MAP
+            )
+            return parsed_example[self._input_feature_key]
+
+        serialized_examples = tf.cast(serialized_examples, tf.string)
+        return tf.map_fn(get_feature, serialized_examples)
 
 
 class Reshaper(keras.layers.Layer):
-  """A Keras layer that reshapes the input."""
+    """A Keras layer that reshapes the input."""
 
-  def call(self, inputs):
-    return tf.reshape(inputs, (1, 32))
+    def call(self, inputs):
+        return tf.reshape(inputs, (1, 32))
 
 
 class Caster(keras.layers.Layer):
-  """A Keras layer that reshapes the input."""
+    """A Keras layer that reshapes the input."""
 
-  def call(self, inputs):
-    return tf.cast(inputs, tf.float32)
+    def call(self, inputs):
+        return tf.cast(inputs, tf.float32)
 
 
 def get_example_model(input_feature_key: str):
-  """Returns a Keras model for testing."""
-  parser = ExampleParser(input_feature_key)
-  text_vectorization = keras.layers.TextVectorization(
-      max_tokens=32,
-      output_mode='int',
-      output_sequence_length=32,
-  )
-  text_vectorization.adapt(
-      ['nontoxic', 'toxic comment', 'test comment', 'abc', 'abcdef', 'random']
-  )
-  dense1 = keras.layers.Dense(
-      32,
-      activation=None,
-      use_bias=True,
-      kernel_initializer='glorot_uniform',
-      bias_initializer='zeros',
-  )
-  dense2 = keras.layers.Dense(
-      1,
-      activation=None,
-      use_bias=False,
-      kernel_initializer='glorot_uniform',
-      bias_initializer='zeros',
-  )
+    """Returns a Keras model for testing."""
+    parser = ExampleParser(input_feature_key)
+    text_vectorization = keras.layers.TextVectorization(
+        max_tokens=32,
+        output_mode="int",
+        output_sequence_length=32,
+    )
+    text_vectorization.adapt(
+        ["nontoxic", "toxic comment", "test comment", "abc", "abcdef", "random"]
+    )
+    dense1 = keras.layers.Dense(
+        32,
+        activation=None,
+        use_bias=True,
+        kernel_initializer="glorot_uniform",
+        bias_initializer="zeros",
+    )
+    dense2 = keras.layers.Dense(
+        1,
+        activation=None,
+        use_bias=False,
+        kernel_initializer="glorot_uniform",
+        bias_initializer="zeros",
+    )
 
-  inputs = tf.keras.Input(shape=(), dtype=tf.string)
-  parsed_example = parser(inputs)
-  text_vector = text_vectorization(parsed_example)
-  text_vector = Reshaper()(text_vector)
-  text_vector = Caster()(text_vector)
-  output1 = dense1(text_vector)
-  output2 = dense2(output1)
-  return tf.keras.Model(inputs=inputs, outputs=output2)
+    inputs = tf.keras.Input(shape=(), dtype=tf.string)
+    parsed_example = parser(inputs)
+    text_vector = text_vectorization(parsed_example)
+    text_vector = Reshaper()(text_vector)
+    text_vector = Caster()(text_vector)
+    output1 = dense1(text_vector)
+    output2 = dense2(output1)
+    return tf.keras.Model(inputs=inputs, outputs=output2)
 
 
 def evaluate_model(
@@ -114,23 +113,23 @@ def evaluate_model(
     tfma_eval_result_path,
     eval_config,
 ):
-  """Evaluate Model using Tensorflow Model Analysis.
+    """Evaluate Model using Tensorflow Model Analysis.
 
-  Args:
-    classifier_model_path: Trained classifier model to be evaluted.
-    validate_tf_file_path: File containing validation TFRecordDataset.
-    tfma_eval_result_path: Path to export tfma-related eval path.
-    eval_config: tfma eval_config.
-  """
+    Args:
+    ----
+      classifier_model_path: Trained classifier model to be evaluted.
+      validate_tf_file_path: File containing validation TFRecordDataset.
+      tfma_eval_result_path: Path to export tfma-related eval path.
+      eval_config: tfma eval_config.
+    """
+    eval_shared_model = tfma.default_eval_shared_model(
+        eval_saved_model_path=classifier_model_path, eval_config=eval_config
+    )
 
-  eval_shared_model = tfma.default_eval_shared_model(
-      eval_saved_model_path=classifier_model_path, eval_config=eval_config
-  )
-
-  # Run the fairness evaluation.
-  tfma.run_model_analysis(
-      eval_shared_model=eval_shared_model,
-      data_location=validate_tf_file_path,
-      output_path=tfma_eval_result_path,
-      eval_config=eval_config,
-  )
+    # Run the fairness evaluation.
+    tfma.run_model_analysis(
+        eval_shared_model=eval_shared_model,
+        data_location=validate_tf_file_path,
+        output_path=tfma_eval_result_path,
+        eval_config=eval_config,
+    )
